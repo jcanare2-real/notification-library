38b15c8c5e1caf7470fca9d9ead33a5a49f27332
Juan Pacheco <juan.pacheco@streambe.com>
2026-01-28 06:52:13 -0400

    Initial commit (Clean structure)

---
+++ src/main/java/com/seek/notification/NotificationLibraryDemo.java
@@ -0,0 +1,12 @@
+package com.seek.notification;
+
+/**
+ * Clase principal de demostración para validación de arquitectura inicial.
+ * Muestra el cumplimiento de ser agnóstica a frameworks y el uso de Java 21.
+ */
+public class NotificationLibraryDemo {
+
+    public static void main(String[] args) {
+        System.out.println("\"Iniciando demostración de la Librería de Notificaciones...\"");
+    }
+}
